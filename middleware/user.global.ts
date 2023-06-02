2c5b2c8c0da0e556e6ca8dca52e3c075d39d1467
smirnov_d <dim-on-94@mail.ru>
2023-06-02 10:21:53 +0300

    restore middleware

---
+++ middleware/user.global.ts
@@ -0,0 +1,10 @@
+export default defineNuxtRouteMiddleware(async () => {
+  const config = useRuntimeConfig()
+  if (config.public.auth?.prefetchUser) {
+    const { getToken, currentUser, fetchCurrentUser } = useAuth();
+
+    if (getToken() && !currentUser.value) {
+      await fetchCurrentUser();
+    }
+  }
+})
